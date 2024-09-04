afdce975183d5366040ae0736fc6f4a0401cf15a
NarendraKumarswami <narendrakumarswami786@gmail.com>
2024-09-04 15:58:15 +0530

    datatypes

---
+++ DataTypessApp/src/com/sarvagna/Main.java
@@ -0,0 +1,27 @@
+package com.sarvagna;
+
+public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		
+//           Wrapper class concept --> for all primitives corrsponding Class is provided in the lang
+		
+//		  Integer n = new Integer(23);   
+		  
+		// due to auto boxing no neeed to do in above way   -- java version 1.5    
+		   Integer n1 = 23;   // here your 23 integral literal value got converted into the Integer object automaticaly by compiler using the concpet of auto boxing
+		  int n2 = 23;
+		  
+		  
+		  int n3 = new Integer(23);   // auto unboxing 
+		      
+		
+		  
+	}
+
+
+
+
+}
---
+++ DataTypessApp/src/com/sarvagna/Student.java
@@ -0,0 +1,8 @@
+package com.sarvagna;
+
+public class Student {
+     
+	  public static int rollNo;
+	  
+	  public static String name; 
+}
